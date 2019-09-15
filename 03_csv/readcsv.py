4420a410ef0e650b18d2169a8d4874675ba5906c
jjshawshaw <jshaw00@stuy.edu>
2019-09-14 22:30:22 -0400

    ingnored first line and made percent values floats

--- 03_csv/readcsv.py
+++ 03_csv/readcsv.py
@@ -1,14 +1,23 @@
 f = open("occupations.csv", 'r')
 dict = {}
+first = True
 for line in f:
-    quoted = False
-    for i in range(len(line)):
-        char = line[i]
-        if char == "\"" and not quoted:
-            quoted = True
-        elif char == "\"" and quoted:
-            quoted = False
-        elif char == "," and not quoted:
-            dict[line[0:i]] = line[i+1:len(line) - 1]
+    if first:
+        first = False
+    else:
+        quoted = False
+        containsQuote = False
+        for i in range(len(line)):
+            char = line[i]
+            if char == "\"" and not quoted:
+                quoted = True
+                containsQuote = True
+            elif char == "\"" and quoted:
+                quoted = False
+            elif char == "," and not quoted:
+                if not containsQuote:
+                    dict[line[0:i]] = float(line[i+1:len(line) - 1])
+                else:
+                    dict[line[1:i-1]] = float(line[i+1:len(line) - 1])
 
 print(dict)
