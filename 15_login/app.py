b4a9a979c2c94faaecb2d60b56bb519c347c6190
jjshawshaw <jshaw00@stuy.edu>
2019-10-03 11:44:06 -0400

    stores if ur logged in or not

--- 15_login/app.py
+++ 15_login/app.py
@@ -1,3 +1,9 @@
+    # Justin Shaw & Tanzim Elahi
+    # SoftDev1 pd1
+    # K15 -- ?
+    # 2019-10-04
+
+
 from flask import Flask
 from flask import render_template
 from flask import request
@@ -7,35 +13,34 @@ import os
 
 # creates flask app
 app = Flask(__name__)
-app.secret_key = os.urandom(32) #secre
-
-head = """
-Justin Shaw & Tanzim Elahi
-SoftDev1 pd1
-K15 -- ?
-2019-10-04
-"""
+app.secret_key = os.urandom(32) #secret key randomly generated
 
 @app.route("/")
 def form():
-    return render_template("form.html")
+    if not "loggedIn" in session:
+        return render_template("form.html")
+    else:
+        return redirect("/welcome")
+
 @app.route("/auth", methods=["POST"])
 def authenticate():
     print(request.form)
     session['username']= request.form.get('username')
     session['password']= request.form.get('password')
     if session['username'] == "fredGang" and session['password'] == "softdev":
+        session['loggedIn'] = True
         return redirect("/welcome")
     else:
+        print(f"{session['username']} {session['password']}")
         return redirect("/error")
 
 @app.route("/welcome")
 def welcome():
-    return "Welcome"
+    return render_template("welcome.html")
 
 @app.route("/error")
 def error():
-    return "Wrong username or password"
+    return render_template("error.html")
 
 
 if __name__ == "__main__":
