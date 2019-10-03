d72643883853922bb8a37bcd71ec53c81e142359
jjshawshaw <jshaw00@stuy.edu>
2019-10-03 08:40:50 -0400

    started wortking with jinja inheritence

--- 15_login/app.py
+++ 15_login/app.py
@@ -5,9 +5,9 @@ from flask import redirect
 from flask import session
 import os
 
+# creates flask app
 app = Flask(__name__)
-app.secret_key = os.urandom(32)
-lIn = False
+app.secret_key = os.urandom(32) #secre
 
 head = """
 Justin Shaw & Tanzim Elahi
@@ -18,17 +18,17 @@ K15 -- ?
 
 @app.route("/")
 def form():
-    return render_template("form.html",
-                               header = head)
-@app.route("/auth")
+    return render_template("form.html")
+@app.route("/auth", methods=["POST"])
 def authenticate():
-        session['username']= request.args['username']
-        session['password']= request.args['password']
-        if session['username'] == "fredGang" and session['password'] == "softdev":
-            return redirect("/welcome")
-        else:
-            return redirect("/error")
-            
+    print(request.form)
+    session['username']= request.form.get('username')
+    session['password']= request.form.get('password')
+    if session['username'] == "fredGang" and session['password'] == "softdev":
+        return redirect("/welcome")
+    else:
+        return redirect("/error")
+
 @app.route("/welcome")
 def welcome():
     return "Welcome"
