6f644a7663e12465ccb0f327119e531d3edc37b8
jjshawshaw <jshaw00@stuy.edu>
2019-10-03 16:22:59 -0400

    commented code

--- 15_login/app.py
+++ 15_login/app.py
@@ -1,7 +1,8 @@
-    # Justin Shaw & Tanzim Elahi
-    # SoftDev1 pd1
-    # K15 -- ?
-    # 2019-10-04
+
+# Justin Shaw & Tanzim Elahi
+# SoftDev1 pd1
+# K15 -- ?
+# 2019-10-04
 
 
 from flask import Flask
@@ -16,43 +17,47 @@ import os
 app = Flask(__name__)
 app.secret_key = os.urandom(32) #secret key randomly generated
 
+## Home route
 @app.route("/")
 def form():
-    if not "loggedIn" in session:
-        return render_template("form.html")
+    if not "loggedIn" in session: #checks in the user is loggedIn
+        return render_template("form.html") #renders login page
     else:
         return redirect("/welcome")
 
+## Authorization for form input
 @app.route("/auth", methods=["POST"])
-def authenticate():
+def authenticate(): #checks username and password
     print(request.form)
-    session['username']= request.form.get('username')
+    session['username']= request.form.get('username') #saves information in session
     session['password']= request.form.get('password')
-    if session['username'] == "fredGang" and session['password'] == "softdev":
-        session['loggedIn'] = True
+    if session['username'] == "fredGang" and session['password'] == "softdev": #checks if username and password are correct
+        session['loggedIn'] = True #stores the fact that the user is logged in in the session
         return redirect("/welcome")
     else:
-        if session['username'] != "fredGang":
+        if session['username'] != "fredGang": #flashes messages clarifying username and password errors
             flash("Incorrect username")
         if session['password'] != "softdev":
             flash("Incorrect password")
-        print(f"{session['username']} {session['password']}")
         return redirect("/error")
 
+## Welcome page
 @app.route("/welcome")
 def welcome():
-    return render_template("welcome.html")
+    return render_template("welcome.html") #renders a welcome page
 
+## Error page
 @app.route("/error")
 def error():
-    return render_template("error.html")
+    return render_template("error.html") #renders an error page with flashed messages
 
-@app.route("/logout")
+## Logout from session
+@app.route("/logout") #removes username and password from the session and indicates the user is logged out
 def logout():
     session.pop('username')
     session.pop('password')
     session.pop('loggedIn')
-    return redirect("/")
+    return redirect("/") #returns to the login page
 
 
 if __name__ == "__main__":
