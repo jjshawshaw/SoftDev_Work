050d55856a1ccaccc3e371398b916e5dd187d63a
jjshawshaw <jshaw00@stuy.edu>
2019-10-10 08:21:37 -0400

    changed DB name to discobandit

--- 17_csv2db/db_builder.py
+++ 17_csv2db/db_builder.py
@@ -7,7 +7,7 @@ import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
 
 
-DB_FILE="studentDB.db"
+DB_FILE="discobandit.db"
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops
 
