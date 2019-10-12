bc8fb588c0716285f30319f5b5cf386fb8d36eca
jjshawshaw <jshaw00@stuy.edu>
2019-10-12 18:58:21 -0400

    calculates avgs and stores in a table

--- 18_db-nmcrnch/stu_mean.py
+++ 18_db-nmcrnch/stu_mean.py
@@ -1,10 +1,11 @@
 #Justin Shaw
 #SoftDev1 pd1
-#K17 -- No Trouble
-#2019-10-10
+#K18 -- Average
+#2019-10-15
 
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
+import statistics
 
 
 DB_FILE="discobandit.db"
@@ -13,9 +14,24 @@ c = db.cursor()               #facilitate db ops
 
 #==========================================================
 
+#access the students and courses tables from the database
 data = c.execute("SELECT name, students.id, mark FROM students, courses WHERE students.id = courses.id;")
-for row in data.fetchall():
-    print(row)
+grades = {} #creates empty dictionaries to store grades and ids
+ids = {}
+for row in data.fetchall(): #fetches selected data from database
+    if row[0] not in grades: #addes grades to list inside dictionary
+        grades[row[0]] = [row[2],]
+    else:
+        grades[row[0]].append(row[2])
+    if row[1] not in ids: #stores student ids inside dictionary
+        ids[row[0]] = row[1]
+print("Averages")
+c.execute("CREATE TABLE IF NOT EXISTS stu_avg(name TEXT, id INTEGER, avg INTEGER);") #creates a table to store averages
+for student in grades:
+    print(f"{student} (id: {ids[student]}): {statistics.mean(grades[student]):.2f}") #prints averages and ids
+    c.execute(f"INSERT INTO courses VALUES (\"{student}\", {ids[student]}, {statistics.mean(grades[student]):.2f});")
+    #inserts averages and ids into stu_avg table
+
 
 #==========================================================
 
