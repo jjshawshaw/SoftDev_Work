974a115a2887f68b4ec93f63ea86603097e6dceb
jjshawshaw <jshaw00@stuy.edu>
2019-11-12 19:03:21 -0500

    displays image n explanation from api request

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -1,14 +1,19 @@
 from flask import Flask, render_template
 from flask import request
-import
+import urllib, json
 
 app = Flask(__name__)
-
+api_key = "Xuy21dR3DcoLW4fSZUPOYQraMEyNr4AR4wBDHRKZ"
+url = f"https://api.nasa.gov/planetary/apod?date=2019-10-31&api_key={api_key}"
 
 @app.route("/")
-def hello_world():
-    print(__name__)
-    return "uwu"
+def api_request():
+    request = urllib.request.urlopen(url).read()
+    print(request)
+    parsed = json.loads(request)
+    print(parsed)
+    return render_template("api_request.html", to_render=parsed)
+
 
 
 if __name__ == "__main__":
