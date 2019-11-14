0d87776f59a55bc656bb4039cbd899f78af33a87
jjshawshaw <jshaw00@stuy.edu>
2019-11-14 01:02:06 -0500

    restrio update

--- 25_restrio/app.py
+++ 25_restrio/app.py
@@ -1,3 +1,8 @@
+# Justin & Yaru
+# SoftDev1 pd01
+# K#25 -- Getting More REST
+# 2019-11-13
+
 from flask import Flask, render_template
 from flask import request
 import urllib, json
@@ -45,6 +50,30 @@ def randm_api_request():
     print(parsed)
     return render_template("randm.html", to_render=parsed, title="Rick and Morty API")
 
+@app.route( '/IPaddress')
+def ip_location_request():
+    url = 'http://ip-api.com/json/64.09.1.2'
+    request = urllib.request.urlopen( url).read()
+    print( request)
+    parsed = json.loads( request)
+    print( parsed)
+    return render_template( 'ip.html', to_render = parsed, title = "IP API")
+
+@app.route('/superhero')
+def _request():
+    url = 'http://superheroapi.com/api.php/2503373653110667/720'
+    hdr = { # ivan did this 
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+        'Accept-Encoding': 'none',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'Connection': 'keep-alive'
+    }
+    req = urllib.request.Request( url, headers=hdr)
+    data = json.loads( urllib.request.urlopen(req).read())
+    return render_template( 'hero.html', to_render=data, title="Hero API")
+    
 
 if __name__ == "__main__":
     app.debug = True
