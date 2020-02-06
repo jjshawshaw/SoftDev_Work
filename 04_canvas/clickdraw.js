f3012e21725501bdd39fb9db1ed40c945444ee4d
jjshawshaw <jshaw00@stuy.edu>
2020-02-05 23:25:52 -0500

    fixed arcs

--- 04_canvas/clickdraw.js
+++ 04_canvas/clickdraw.js
@@ -1,4 +1,9 @@
-var mode = "box";
+//Pratham Rawat and Justin Shaw
+//SoftDev pd1
+//K04 -- I see a red door
+//2020 - 02 - 05
+
+var mode = "circle";
 const BOX_SIDE_LENGTH = 50;
 const CIRCLE_RADIUS = 5;
 
@@ -11,8 +16,24 @@ c.addEventListener("click", function(e){
   if ( mode == "box" ){
     ctx.fillRect(event.clientX - rect.x, e.clientY - rect.y , BOX_SIDE_LENGTH, BOX_SIDE_LENGTH);
   } else {
+    ctx.beginPath();
     ctx.arc(e.clientX - rect.x, e.clientY - rect.y, CIRCLE_RADIUS, 0, Math.PI * 2);
     ctx.closePath();
     ctx.fill();
   }
+});
+
+document.getElementById('toggle').addEventListener("click", function() {
+  console.log("togl");
+  if(mode == "box") {
+    mode = "circle"
+  } else {
+    mode = "box"
+  }
+});
+
+
+document.getElementById('clear').addEventListener("click", function() {
+  console.log("clr");
+  ctx.clearRect(0, 0, c.height, c.width);
 });
