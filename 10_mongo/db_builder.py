d03b734c2085e03ce0007e3200e76dc0203a2fc3
jshaw <jshaw00@stuy.edu>
2020-03-03 03:33:58 +0000

    databaseffjfk

--- 10_mongo/db_builder.py
+++ 10_mongo/db_builder.py
@@ -6,5 +6,15 @@ with open("subway_entrances.json") as data_file:
     data = json.loads(data_file.read())
 
 
-for i in data["meta"]["view"]["columns"]:
-    print(i)
+fields = []
+for field in data["meta"]["view"]["columns"]:
+    fields.append(field["name"])
+
+
+client = MongoClient('localhost', 27017)
+db = client["entrances"]
+for entry in data["data"]:
+    ins = {}
+    for i in range(len(fields)):
+        ins[fields[i]] = entry[i]
+    db.entranceCollection.insert_one(ins)
