42ea5d9e0448be64c656f04834b3b17d0b03d0b3
jshaw <jshaw00@stuy.edu>
2020-03-04 08:05:22 +0000

    added comments

--- 10_mongo/db_builder.py
+++ 10_mongo/db_builder.py
@@ -1,21 +1,27 @@
+#Justin Shaw and Clement Chan
+#SoftDev p1
+#K10 - Import/Export Bank
+#2002-03-04
+
 import json
 from pymongo import MongoClient
 
+#open json file and read
 data = {}
 with open("subway_entrances.json") as data_file:
     data = json.loads(data_file.read())
 
-
+#use the meta section of the json file to detirmine fields
 fields = []
 for field in data["meta"]["view"]["columns"]:
     fields.append(field["name"])
 
-
+#opens a client and creates a database called entrances
 client = MongoClient('localhost', 27017)
 db = client["entrances"]
 for entry in data["data"]:
     ins = {}
     for i in range(len(fields)):
-        ins[fields[i]] = entry[i]
-    ins[fields[11]] = ins[fields[11]].strip("POINT (").strip(")").split()
-    db.entranceCollection.insert_one(ins)
+        ins[fields[i]] = entry[i] #use fields to input data
+    ins[fields[11]] = ins[fields[11]].strip("POINT (").strip(")").split() #save coordinates as an array
+    db.entranceCollection.insert_one(ins) #puts information into a collection called entranceCollection
